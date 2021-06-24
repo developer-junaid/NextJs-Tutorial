6ca13ec03de543a3f6a974437a140e7b2fc2af8a
developer-junaid <qjunaid623@gmail.com>
2021-06-24 17:19:28 +0500

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Link
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>Ooopss...</h1>
+      <h2>That page cannot be found.</h2>
+      <p>
+        Go back to the{" "}
+        <Link href="/">
+          <a>Homepage</a>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
