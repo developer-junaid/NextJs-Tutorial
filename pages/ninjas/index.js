02f896b7dc2aa242cdc09f912ffc83ce8865008b
developer-junaid <qjunaid623@gmail.com>
2021-06-24 17:40:46 +0500

    fetching data and showing

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,39 @@
 import React from "react";
 
-const Ninjas = () => {
+// Styles
+import styles from "../../styles/Ninjas.module.css";
+
+// Runs first // Fetch Data here
+export const getStaticProps = async () => {
+  // Runs at build-time
+  // Create Fetch request
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await response.json();
+
+  // Return data
+  return {
+    props: {
+      // Attached to props
+      ninjas: data,
+    },
+  };
+};
+
+// Runs After
+const Ninjas = ({ ninjas }) => {
+  // Fetch Data
+
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas &&
+        ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
     </div>
   );
 };
