ee16fefd093bbb920e03c420d0aaf951b50264a4
developer-junaid <qjunaid623@gmail.com>
2021-06-24 17:31:44 +0500

    added metadata to pages

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,31 @@
 import React from "react";
 
+// Next
+import Head from "next/head";
+
 const about = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-        commodi ipsum in pariatur soluta quam, omnis tenetur nisi aspernatur
-        debitis blanditiis provident magni error expedita eveniet voluptas saepe
-        adipisci cumque?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-        commodi ipsum in pariatur soluta quam, omnis tenetur nisi aspernatur
-        debitis blanditiis provident magni error expedita eveniet voluptas saepe
-        adipisci cumque?
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+          commodi ipsum in pariatur soluta quam, omnis tenetur nisi aspernatur
+          debitis blanditiis provident magni error expedita eveniet voluptas
+          saepe adipisci cumque?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+          commodi ipsum in pariatur soluta quam, omnis tenetur nisi aspernatur
+          debitis blanditiis provident magni error expedita eveniet voluptas
+          saepe adipisci cumque?
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 
-// Components
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-
 // Styles for this page
 import styles from "../styles/Home.module.css";
 
@@ -13,23 +9,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-        commodi ipsum in pariatur soluta quam, omnis tenetur nisi aspernatur
-        debitis blanditiis provident magni error expedita eveniet voluptas saepe
-        adipisci cumque?
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-        commodi ipsum in pariatur soluta quam, omnis tenetur nisi aspernatur
-        debitis blanditiis provident magni error expedita eveniet voluptas saepe
-        adipisci cumque?
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+          commodi ipsum in pariatur soluta quam, omnis tenetur nisi aspernatur
+          debitis blanditiis provident magni error expedita eveniet voluptas
+          saepe adipisci cumque?
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+          commodi ipsum in pariatur soluta quam, omnis tenetur nisi aspernatur
+          debitis blanditiis provident magni error expedita eveniet voluptas
+          saepe adipisci cumque?
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
