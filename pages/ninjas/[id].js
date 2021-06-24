2d9208a6b60f96c6b3dacd162876e00dbf8315ee
developer-junaid <qjunaid623@gmail.com>
2021-06-24 18:00:13 +0500

    Template pages and getting and showing data

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,10 +1,46 @@
 // Dynamic component
 import React from "react";
 
-const Details = () => {
+// Runs at build time
+export const getStaticPaths = async () => {
+  // Fetch all Data and get ids
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await response.json();
+
+  // Get ids
+  const paths = data.map((user) => {
+    return { params: { id: user.id.toString() } };
+  });
+
+  // Return
+  return {
+    paths, // Generate this page 10 times
+    fallback: false, // for fallback pages, if it doesn't exist
+  };
+};
+
+// Get Static Props // Runs for every page
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  ); //Get single user
+  const data = await response.json();
+
+  // Return
+  return {
+    props: { ninja: data },
+  };
+};
+
+// Component
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };
