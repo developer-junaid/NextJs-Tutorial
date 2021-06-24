92cd3e30372f6ee222620293cea0642d2cea09b2
developer-junaid <qjunaid623@gmail.com>
2021-06-24 16:42:18 +0500

    creating navbar and footer components

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>Copyright 2021 Ninja List</div>;
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
@@ -18,6 +21,7 @@ export default function Home() {
         debitis blanditiis provident magni error expedita eveniet voluptas saepe
         adipisci cumque?
       </p>
+      <Footer />
     </div>
   );
 }
